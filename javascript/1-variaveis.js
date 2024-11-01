2f38416cdb6fd417fd8057782fef2866e512fcf3
02514860105 <victor.costa@tjce.jus.br>
2024-10-31 21:41:46 -0300

    build: Hello word! Starting a new class of React - basics to advanced

---
+++ javascript/1-variaveis.js
@@ -0,0 +1,12 @@
+//var numero1 = 5;
+//console.log(numero1)
+
+let numero2 = 10;
+console.log(numero2);
+
+
+const numero3 = 15;
+console.log(numero3);
+
+numero3+= 2;
+console.log(numero3);
---
+++ javascript/modulos/pessoa.mjs
@@ -0,0 +1,7 @@
+//Exportações padrão
+const pessoa= {
+  nome: "João",
+  idade: 26
+}
+
+export default pessoa
