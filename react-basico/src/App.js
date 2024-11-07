ddff7785a1916036423162c52cded3db5b1827ff
02514860105 <victor.costa@tjce.jus.br>
2024-11-06 21:11:34 -0300

    feat: trabalhando com propiedades - props

--- react-basico/src/App.js
+++ react-basico/src/App.js
@@ -6,9 +6,12 @@ function App() {
   return (
     <div className="App">
       <h1>Meu Projeto</h1>
-      <Comentario />
-      <Comentario />
-      <Comentario />
+      <Comentario nome='João' email='joao@mailcom' data={new Date(2024, 11, 6)}>
+        Olá, tudo bem?
+      </Comentario>
+      <Comentario nome='Maria' email='maria@mailcom' data={new Date(2024, 11, 4)}>
+        Olá, tudo bem sim
+      </Comentario>
     </div>
   );
 }
